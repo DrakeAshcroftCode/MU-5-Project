5db441f1933dce9d77ab5d32b4dc72f4dcbc3d65
BlueKanary <gwynnek@yahoo.com>
2026-01-29 14:02:41 -0500

    data model

---
+++ MU5PrototypeProject/MU5PrototypeProject/Models/Client.cs
@@ -0,0 +1,16 @@
+﻿namespace MU5PrototypeProject.Models
+{
+    public class Client
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public string Phone {  get; set; }
+        public string Email { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public ICollection<Session> Sessions { get; set; } = new HashSet<Session>();
+    }
+}
---
+++ MU5PrototypeProject/MU5PrototypeProject/Models/Trainer.cs
@@ -0,0 +1,11 @@
+﻿namespace MU5PrototypeProject.Models
+{
+    public class Trainer
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public ICollection<Session> Sessions { get; set; } = new HashSet<Session>();
+    }
+}
