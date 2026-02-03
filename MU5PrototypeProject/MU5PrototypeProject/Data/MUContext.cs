5d9323031821a51ed662534d296fb9af1e182df6
Saim-23 <akhtarsaim8@gmail.com>
2026-02-02 23:26:33 -0800

    Created MUContext.cs intalized data models to DbSet. Added OnModeCreating. Created _CommonCommand.txt file, Changed background color to make the title more visible. Downloaded and added connections to SQLite

---
+++ MU5PrototypeProject/MU5PrototypeProject/Data/MUContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MU5PrototypeProject.Models;
+using System.Numerics;
+
+namespace MU5PrototypeProject.Data
+{
+    public class MUContext : DbContext
+    {
+        public MUContext(DbContextOptions<MUContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Trainer> Trainers { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<SessionExercise> SessionExercises { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Trainer-Session relationship
+            modelBuilder.Entity<Trainer>()
+               .HasMany(t => t.Sessions)
+               .WithOne(s => s.Trainer)
+               .HasForeignKey(s => s.TrainerID)
+               .OnDelete(DeleteBehavior.Restrict); // Prevent deleting trainer with sessions
+
+            //Client-Session relationship
+            modelBuilder.Entity<Client>()
+                .HasMany(c => c.Sessions)
+                .WithOne(s => s.Client)
+                .HasForeignKey(s => s.ClientID)
+                .OnDelete(DeleteBehavior.Restrict); // Prevent deleting client with sessions
+
+        }
+
+    }
+
+    
+}
