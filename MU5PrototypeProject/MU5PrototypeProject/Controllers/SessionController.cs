c4dfcc0820dec94ceda57bef11a6cee91d5eef1d
nasra-ib <nibrahim4@ncstudents.niagaracollege.ca>
2026-02-08 10:28:12 -0500

    Scaffolded Client and Session Controllers. "Clients" and "Sessions" should be deleted but just leave them for now.

---
+++ MU5PrototypeProject/MU5PrototypeProject/Controllers/SessionController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MU5PrototypeProject.Data;
+using MU5PrototypeProject.Models;
+
+namespace MU5PrototypeProject.Controllers
+{
+    public class SessionController : Controller
+    {
+        private readonly MUContext _context;
+
+        public SessionController(MUContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Session
+        public async Task<IActionResult> Index()
+        {
+            var mUContext = _context.Sessions.Include(s => s.Client).Include(s => s.Trainer);
+            return View(await mUContext.ToListAsync());
+        }
+
+        // GET: Session/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var session = await _context.Sessions
+                .Include(s => s.Client)
+                .Include(s => s.Trainer)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+
+            return View(session);
+        }
+
+        // GET: Session/Create
+        public IActionResult Create()
+        {
+            ViewData["ClientID"] = new SelectList(_context.Clients, "ID", "FirstName");
+            ViewData["TrainerID"] = new SelectList(_context.Trainers, "ID", "FirstName");
+            return View();
+        }
+
+        // POST: Session/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,SessionDate,CreatedAt,SessionsPerWeekRecommended,IsArchived,TrainerID,ClientID")] Session session)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(session);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClientID"] = new SelectList(_context.Clients, "ID", "FirstName", session.ClientID);
+            ViewData["TrainerID"] = new SelectList(_context.Trainers, "ID", "FirstName", session.TrainerID);
+            return View(session);
+        }
+
+        // GET: Session/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var session = await _context.Sessions.FindAsync(id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClientID"] = new SelectList(_context.Clients, "ID", "FirstName", session.ClientID);
+            ViewData["TrainerID"] = new SelectList(_context.Trainers, "ID", "FirstName", session.TrainerID);
+            return View(session);
+        }
+
+        // POST: Session/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,SessionDate,CreatedAt,SessionsPerWeekRecommended,IsArchived,TrainerID,ClientID")] Session session)
+        {
+            if (id != session.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(session);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SessionExists(session.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClientID"] = new SelectList(_context.Clients, "ID", "FirstName", session.ClientID);
+            ViewData["TrainerID"] = new SelectList(_context.Trainers, "ID", "FirstName", session.TrainerID);
+            return View(session);
+        }
+
+        // GET: Session/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var session = await _context.Sessions
+                .Include(s => s.Client)
+                .Include(s => s.Trainer)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+
+            return View(session);
+        }
+
+        // POST: Session/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var session = await _context.Sessions.FindAsync(id);
+            if (session != null)
+            {
+                _context.Sessions.Remove(session);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SessionExists(int id)
+        {
+            return _context.Sessions.Any(e => e.ID == id);
+        }
+    }
+}
