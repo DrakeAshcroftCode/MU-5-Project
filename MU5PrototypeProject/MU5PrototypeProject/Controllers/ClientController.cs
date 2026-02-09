f622a585af1314af9d50df784dfc1c7141208ebf
Saim-23 <akhtarsaim8@gmail.com>
2026-02-08 23:06:44 -0800

    Client Controller Modifications (try catch blocks and TryUpdateModel)

--- MU5PrototypeProject/MU5PrototypeProject/Controllers/ClientController.cs
+++ MU5PrototypeProject/MU5PrototypeProject/Controllers/ClientController.cs
@@ -1,12 +1,13 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using MU5PrototypeProject.Data;
+using MU5PrototypeProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using MU5PrototypeProject.Data;
-using MU5PrototypeProject.Models;
 
 namespace MU5PrototypeProject.Controllers
 {
@@ -22,8 +23,20 @@ namespace MU5PrototypeProject.Controllers
         // GET: Client
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Clients.ToListAsync());
+            try
+            {
+                var clients = await _context.Clients
+                        .AsNoTracking()
+                        .ToListAsync();
+
+                return View(clients);
+            }
+            catch (Exception)
+            {
+                return Problem("Unable to load clients. Please try again later.");
+            }
         }
+
 
         // GET: Client/Details/5
         public async Task<IActionResult> Details(int? id)
@@ -34,6 +47,7 @@ namespace MU5PrototypeProject.Controllers
             }
 
             var client = await _context.Clients
+                .AsNoTracking()
                 .FirstOrDefaultAsync(m => m.ID == id);
             if (client == null)
             {
@@ -56,12 +70,26 @@ namespace MU5PrototypeProject.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("ID,FirstName,LastName,DOB,Phone,Email,ClientFolderUrl,CreatedAt,IsArchived")] Client client)
         {
-            if (ModelState.IsValid)
+            try
             {
-                _context.Add(client);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                if (ModelState.IsValid)
+                {
+                    _context.Add(client);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
             }
+            catch (RetryLimitExceededException)
+            {
+                ModelState.AddModelError("", "Unable to save changes after multiple attempts. " +
+                    "Try again, and if the problem persists, see your system administrator.");
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+
+
             return View(client);
         }
 
@@ -86,23 +114,28 @@ namespace MU5PrototypeProject.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,FirstName,LastName,DOB,Phone,Email,ClientFolderUrl,CreatedAt,IsArchived")] Client client)
+        public async Task<IActionResult> Edit(int id)
         {
-            if (id != client.ID)
-            {
-                return NotFound();
-            }
+            //Go get the client to update from the database
+            var clientToUpdate = await _context.Clients.FirstOrDefaultAsync(c => c.ID == id);
 
-            if (ModelState.IsValid)
+            //Check if the client exists
+            if (clientToUpdate == null)
+               {
+                    return NotFound();
+               }
+
+            if (await TryUpdateModelAsync(clientToUpdate, "", c => c.FirstName, c => c.LastName, c => c.DOB, 
+                c => c.Phone, c => c.Email, c => c.ClientFolderUrl, c => c.CreatedAt, c => c.IsArchived))  
             {
                 try
                 {
-                    _context.Update(client);
                     await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!ClientExists(client.ID))
+                    if (!ClientExists(clientToUpdate.ID))
                     {
                         return NotFound();
                     }
@@ -111,9 +144,17 @@ namespace MU5PrototypeProject.Controllers
                         throw;
                     }
                 }
-                return RedirectToAction(nameof(Index));
+                catch (RetryLimitExceededException)
+                {
+                    ModelState.AddModelError("", "Unable to save changes after multiple attempts. " +
+                        "Try again, and if the problem persists, see your system administrator.");
+                }
+                catch (DbUpdateException)
+                {
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+                }
             }
-            return View(client);
+            return View(clientToUpdate);
         }
 
         // GET: Client/Delete/5
@@ -125,6 +166,7 @@ namespace MU5PrototypeProject.Controllers
             }
 
             var client = await _context.Clients
+                .AsNoTracking()
                 .FirstOrDefaultAsync(m => m.ID == id);
             if (client == null)
             {
@@ -139,14 +181,24 @@ namespace MU5PrototypeProject.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var client = await _context.Clients.FindAsync(id);
-            if (client != null)
+            var client = await _context.Clients
+                .FirstOrDefaultAsync(m => m.ID == id);
+            try
             {
-                _context.Clients.Remove(client);
-            }
+                if (client != null)
+                {
+                    _context.Clients.Remove(client);
+                }
 
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException)
+            {
+                //Log the error (uncomment ex variable name and write a log.)   
+                ModelState.AddModelError("", "Unable to delete client. Try again, and if the problem persists see your system administrator.");
+            }
+           return View(client);
         }
 
         private bool ClientExists(int id)
