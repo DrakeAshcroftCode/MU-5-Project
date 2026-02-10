b3d84371c407a07cd8f59b6996357a7243763cf6
nasra-ib <nibrahim4@ncstudents.niagaracollege.ca>
2026-02-09 21:00:20 -0500

    Added filtering , but I can't get Search by SessionDate to work.

--- MU5PrototypeProject/MU5PrototypeProject/Controllers/SessionController.cs
+++ MU5PrototypeProject/MU5PrototypeProject/Controllers/SessionController.cs
@@ -16,14 +16,56 @@ namespace MU5PrototypeProject.Controllers
         }
 
         // GET: Session
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string? SearchSessionDate, int? TrainerID, int? ClientID, string actionButton, string sortDirection = "asc", string sortField = "Session"/*, int? page, int? pageSizeID*/)
         {
+
+            //List of sort options.
+            //NOTE: make sure this array has matching values to the column headings
+            string[] sortOptions = new[] { "Client", "Trainer", "SessionDate" };
+
+            ////Count the number of filters applied - start by assuming no filters
+            //ViewData["Filtering"] = "btn-outline-secondary";
+            //int numberFilters = 0;
+            ////Then in each "test" for filtering, add to the count of Filters applied
+
+            PopulateDropDownLists();//Data for Session filter for ddl
+
             var sessions = _context.Sessions
                 .Include(s => s.Client)
                 .Include(s => s.Trainer)
-                .AsNoTracking(); ;
+                .AsNoTracking();
+
+            //Add as many filters as needed 
+            if (ClientID.HasValue)
+            {
+                sessions = sessions.Where(s => s.ClientID == ClientID);
+            }
+            if (TrainerID.HasValue)
+            {
+                sessions = sessions.Where(p => p.TrainerID == TrainerID);
+            }
+            if (!String.IsNullOrEmpty(SearchSessionDate))
+            {
+                sessions = sessions.Where(p => p.SessionDate.Date.Contains(SearchSessionDate);
+            }
             return View(await sessions.ToListAsync());
+
+            ////Before we sort, see if we have called for a change of filtering or sorting
+            //if (!String.IsNullOrEmpty(actionButton)) //Form Submitted!
+            //{
+            //    //page = 1;//Reset page to start
+
+            //    if (sortOptions.Contains(actionButton))//Change of sort is requested
+            //    {
+            //        if (actionButton == sortField) //Reverse order on same field
+            //        {
+            //            sortDirection = sortDirection == "asc" ? "desc" : "asc";
+            //        }
+            //        sortField = actionButton;//Sort by the button clicked
+            //    }
+            //}
         }
+
 
         // GET: Session/Details/5
         public async Task<IActionResult> Details(int? id)
@@ -36,6 +78,7 @@ namespace MU5PrototypeProject.Controllers
             var session = await _context.Sessions
                 .Include(s => s.Client)
                 .Include(s => s.Trainer)
+                .AsNoTracking() //needed?
                 .FirstOrDefaultAsync(m => m.ID == id);
             if (session == null)
             {
@@ -61,12 +104,21 @@ namespace MU5PrototypeProject.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("ID,SessionDate,CreatedAt,SessionsPerWeekRecommended,IsArchived,TrainerID,ClientID")] Session session)
         {
-            if (ModelState.IsValid)
+            try
             {
-                _context.Add(session);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                if (ModelState.IsValid)
+                {
+                    _context.Add(session);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
             }
+            catch (DbUpdateException dex)
+            {
+                string message = dex.GetBaseException().Message;
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+
             PopulateDropDownLists(session);
             //ViewData["ClientID"] = new SelectList(_context.Clients, "ID", "FirstName", session.ClientID);
             //ViewData["TrainerID"] = new SelectList(_context.Trainers, "ID", "FirstName", session.TrainerID);
@@ -96,37 +148,35 @@ namespace MU5PrototypeProject.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,SessionDate,CreatedAt,SessionsPerWeekRecommended,IsArchived,TrainerID,ClientID")] Session session)
+        public async Task<IActionResult> Edit(int id)
         {
-            if (id != session.ID)
+            //Go get the musician to update
+            var sessionToUpdate = await _context.Sessions.FirstOrDefaultAsync(s => s.ID == id);
+            if (sessionToUpdate == null)
             {
                 return NotFound();
             }
 
-            if (ModelState.IsValid)
+            //Try updating it with value
+            if (await TryUpdateModelAsync<Session>(sessionToUpdate, "",
+                t => t.SessionDate, t => t.CreatedAt, t => t.SessionsPerWeekRecommended,
+                t => t.IsArchived, t => t.TrainerID, t => t.ClientID))
             {
                 try
                 {
-                    _context.Update(session);
                     await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
                 }
-                catch (DbUpdateConcurrencyException)
+                catch (DbUpdateException dex)
                 {
-                    if (!SessionExists(session.ID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                    string messsage = dex.GetBaseException().Message;
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
                 }
-                return RedirectToAction(nameof(Index));
             }
-            PopulateDropDownLists(session);
+            PopulateDropDownLists(sessionToUpdate);
             //ViewData["ClientID"] = new SelectList(_context.Clients, "ID", "FirstName", session.ClientID);
             //ViewData["TrainerID"] = new SelectList(_context.Trainers, "ID", "FirstName", session.TrainerID);
-            return View(session);
+            return View(sessionToUpdate);
         }
 
         // GET: Session/Delete/5
@@ -155,26 +205,43 @@ namespace MU5PrototypeProject.Controllers
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var session = await _context.Sessions.FindAsync(id);
-            if (session != null)
-            {
-                _context.Sessions.Remove(session);
-            }
 
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                if (session != null)
+                {
+                    _context.Sessions.Remove(session);
+                }
+
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException dex)
+            {
+                string messsage = dex.GetBaseException().Message;
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            return View(session);
         }
 
         private void PopulateDropDownLists(Session? session = null)
         {
 
-            var dQuery = from t in _context.Clients
+            var clientObjs = from t in _context.Clients
 
-                         orderby t.FirstName
-                         select t;
+                             orderby t.FirstName
+                             select t;
 
-            ViewData["ClientID"] = new SelectList(dQuery, "FirstName", "LastName", session?.ClientID);
+            ViewData["ClientID"] = new SelectList(clientObjs, nameof(Client.ID), nameof(Client.ClientName));
 
-            ViewData["TrainerID"] = new SelectList(dQuery, "FirstName", "LastName", session?.TrainerID);
+
+
+            var trainerObjs = from t in _context.Trainers
+
+                              orderby t.FirstName
+
+                              select t;
+            ViewData["TrainerID"] = new SelectList(trainerObjs, nameof(Trainer.ID), nameof(Trainer.TrainerName));
 
         }
         private bool SessionExists(int id)
